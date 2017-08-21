9397dae32835e3b9d5d2fa2aa2706434eeaa3cbb
zankia <zankia16@gmail.com>
2017-08-21 11:07:23 +0200

    added more security to resetDB (and kept it for now)

--- app/src/main/java/fr/zankia/stock/view/MainActivity.java
+++ app/src/main/java/fr/zankia/stock/view/MainActivity.java
@@ -1,12 +1,15 @@
 package fr.zankia.stock.view;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import fr.zankia.stock.R;
@@ -29,66 +32,64 @@ public class MainActivity extends Activity {
     }
 
     public void resetDB(View view) {
-        StockDbHelper helper = new StockDbHelper(view.getContext());
-        SQLiteDatabase db = helper.getReadableDatabase();
+        final StockDbHelper helper = new StockDbHelper(view.getContext());
+        final EditText editText = new EditText(this);
 
-        helper.delete(db);
-        helper.onCreate(db);
+        new AlertDialog.Builder(this)
+                .setMessage(getString(R.string.dbWarning) + getString(R.string.validityCheck))
+                .setView(editText)
+                .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(editText.getText().toString().equals(getString(R.string.validityCheck))) {
+                            SQLiteDatabase db = helper.getReadableDatabase();
+                            helper.delete(db);
+                            helper.onCreate(db);
+                            ContentValues values = new ContentValues();
+                            values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 1");
+                            db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
+                            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-2");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
+                            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-3");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
+                            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-4");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
+                            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-5");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
+                            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-6");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
+                            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
+                            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
+                            values = new ContentValues();
+                            values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 2");
+                            db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
+                            db.close();
 
-        ContentValues values = new ContentValues();
-        values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 1");
-        db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
-        values = new ContentValues();
-        values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 2");
-        db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
-        values = new ContentValues();
-        values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 3");
-        db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
-        values = new ContentValues();
-        values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 4");
-        db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
-        values = new ContentValues();
-        values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 5");
-        db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
-        values = new ContentValues();
-        values.put(StockContract.CategoryEntry.COLUMN_NAME_NAME, "Fournisseur 6");
-        db.insert(StockContract.CategoryEntry.TABLE_NAME, null, values);
+                            Log.d("DB", "DB remplie");
 
-        for(int i = 1; i < 11; ++i) {
-            values = new ContentValues();
-            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 1-" + i);
-            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 1");
-            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
-            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
-            values = new ContentValues();
-            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 2-" + i);
-            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 2");
-            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
-            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
-            values = new ContentValues();
-            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 3-" + i);
-            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 3");
-            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
-            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
-            values = new ContentValues();
-            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 4-" + i);
-            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 4");
-            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
-            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
-            values = new ContentValues();
-            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 5-" + i);
-            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 5");
-            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
-            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
-            values = new ContentValues();
-            values.put(StockContract.ProductEntry.COLUMN_NAME_NAME, "Référence 6-" + i);
-            values.put(StockContract.ProductEntry.COLUMN_NAME_CAT, "Fournisseur 6");
-            values.put(StockContract.ProductEntry.COLUMN_NAME_QUANTITY, 0);
-            db.insert(StockContract.ProductEntry.TABLE_NAME, null, values);
-        }
-
-        Log.d("DB", "DB remplie");
-
-        Toast.makeText(getApplicationContext(), R.string.dbToast, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), R.string.dbToast, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 }
